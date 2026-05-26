e62284b064901357fec38f60c7bef2908628363e
kmj0973 <kmj0973@naver.com>
2026-05-26 22:59:05 +0900

    fix: url

--- src/apis/groupApi.js
+++ src/apis/groupApi.js
@@ -1,13 +1,13 @@
 import { instance } from './instance';
 
 export const createGroup = async (form) => {
-  const response = await instance.post('/api/v1/trip-groups', form);
+  const response = await instance.post('/v1/trip-groups', form);
   return response.data;
 };
 
 export const joinGroup = async (invieCode, nickname) => {
   const response = await instance.post(
-    `/api/v1/trip-groups/invite/${invieCode}/join`,
+    `/v1/trip-groups/invite/${invieCode}/join`,
     { nickname },
   );
   return response.data;
